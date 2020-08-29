6d8e8f2d26b6373570606388ed3622e4d0688c1c
Alexander <alexander.ayras@gmail.com>
2020-08-28 22:22:10 -0500

    Creando los Primeros Styled Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Header from './components/Header'
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+	min-width: 600px;
+	margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+	background-color: #fff;
+	padding: 3rem;
+`
 
 function App() {
 	return (
-		<div className='App'>
+		<Contenedor className='App'>
 			<Header titulo='Cotizador de Seguros' />
-		</div>
+			<ContenedorFormulario></ContenedorFormulario>
+		</Contenedor>
 	)
 }
 
